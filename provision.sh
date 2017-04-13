4ee23e8ca78d96ec51f140f4a749cd90cd04b477
Brice Bentler <server4001@gmail.com>
2017-04-12 21:12:43 -0700

    Adds composer, memcached, and bash roles to Ansible provisioner

--- provision.sh
+++ provision.sh
@@ -3,29 +3,5 @@
 export ORIGIN_DIR=/home/vagrant
 
 
-# Install Redis
-sudo apt-get install -y redis-server
-sudo cp /vagrant/config/redis.conf /etc/redis/redis.conf
-sudo service redis-server restart
-sudo update-rc.d redis-server enable
-
-# Install Memcached.
-sudo apt-get install -y memcached
-sudo rm /etc/memcached.conf
-sudo cp /vagrant/config/memcached.conf /etc/memcached.conf
-sudo service memcached restart
-sudo update-rc.d memcached enable
-sudo apt-get install -y php-memcached
-
 # Web root.
 sudo chown -R vagrant:vagrant /var/www
-
-# Install composer
-php /vagrant/config/php/composer-setup.php
-sudo mv composer.phar /usr/local/bin/composer
-
-# Composer packages.
-composer g require psy/psysh:@stable phpunit/phpunit
-
-# Bash.
-cp /vagrant/config/bash/profile /home/vagrant/.profile
